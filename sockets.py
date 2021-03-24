d8b85c0263187246a680846ad1a260788405521b
Qianxi Li <qianxi@ualberta.ca>
2021-03-24 16:58:38 -0700

    first commit

--- sockets.py
+++ sockets.py
@@ -13,6 +13,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+# Copyright 2021 Qianxi Li
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import flask
 from flask import Flask, request
 from flask_sockets import Sockets
@@ -90,10 +104,10 @@ def read_ws(ws,client):
     try:
         while True:
             message = ws.receive()
-            print("message",message)
+            #print("message",message)
             if(message):
                 packet_received = json.loads(message)
-                print("packet_received",packet_received)
+                #print("packet_received",packet_received)
                 key = list(packet_received.keys())[0]
                 myWorld.set(key, list(packet_received.values())[0])
                 #myWorld.update_listeners(key)
@@ -115,7 +129,7 @@ def subscribe_socket(ws):
         while True:
             # block here
             msg = client.get_queue()
-            print("msg",msg)
+            #print("msg",msg)
             ws.send(msg)
     except Exception as e:
         print("WS Error %s" % e)
